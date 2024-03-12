0334b79527c82305fe79b008293afe75654bebea
steffert1 <122401310+5t3ff3rt@users.noreply.github.com>
2024-03-12 23:23:49 +0100

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -1,5 +1,18 @@
-Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v1.3.2691/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile $env:TEMP\winget.msixbundle
-Add-AppxPackage -Path $env:TEMP\winget.msixbundle
+# get latest download url
+$URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+$URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
+        Select-Object -ExpandProperty "assets" |
+        Where-Object "browser_download_url" -Match '.msixbundle' |
+        Select-Object -ExpandProperty "browser_download_url"
+
+# download
+Invoke-WebRequest -Uri $URL -OutFile "Setup.msix" -UseBasicParsing
+
+# install
+Add-AppxPackage -Path "Setup.msix"
+
+# delete file
+Remove-Item "Setup.msix"
 
 # winget script
 Function main() {
