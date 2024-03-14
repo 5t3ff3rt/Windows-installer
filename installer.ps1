215ea936243cab3c09d2fc4dd3775f44d4cf7aef
steffert1 <122401310+5t3ff3rt@users.noreply.github.com>
2024-03-14 23:40:33 +0100

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -2,6 +2,7 @@
 $API_URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
 $DOWNLOAD_URL = $(Invoke-RestMethod $API_URL).assets.browser_download_url |
     Where-Object {$_.EndsWith(".msixbundle")}
+$progressPreference = 'silentlyContinue'
 Invoke-WebRequest -URI $DOWNLOAD_URL -OutFile winget.msixbundle -UseBasicParsing
 Add-AppxPackage winget.msixbundle
 Remove-Item winget.msixbundle
