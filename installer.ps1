593258983a5fe414da00fc35b9cd1d8c62da4da0
steffert1 <122401310+5t3ff3rt@users.noreply.github.com>
2024-03-14 23:16:32 +0100

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -1,16 +1,3 @@
-#$progressPreference = 'silentlyContinue'
-
-# install Microsoft.UI.2.8.6.Xaml
-Invoke-WebRequest `
-    -URI https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.8.6 `
-    -OutFile xaml.zip -UseBasicParsing
-New-Item -ItemType Directory -Path xaml
-Expand-Archive -Path xaml.zip -DestinationPath xaml
-Add-AppxPackage -Path "xaml\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.8.appx"
-Remove-Item xaml.zip
-Remove-Item xaml -Recurse
-
-
 # install latest winget version
 $API_URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
 $DOWNLOAD_URL = $(Invoke-RestMethod $API_URL).assets.browser_download_url |
